f46e7176e4c4a4341416d907bb1e726a9ea45d9e
Martin Ejdestig <mejdestig@luxoft.com>
2019-06-12 12:05:31 +0200

    Add IdSource base class
    Common base for different sources for identifying a user. IdSource:s will
    be grouped together in a IdSource::Group, which is the main class through
    which the rest of the daemon will interface with sources.
    
    Just instantiate a IdSource::Group in Daemon for now. Will actually be used
    in following commit that implements the D-Bus interface.
    
    Signed-off-by: Martin Ejdestig <mejdestig@luxoft.com>

---
+++ src/daemon/id_source.cpp
@@ -0,0 +1,134 @@
+// Copyright (C) 2019 Luxoft Sweden AB
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#include "daemon/id_source.h"
+
+#include <glib.h>
+#include <glibmm.h>
+
+#include <utility>
+
+#include "config.h"
+
+namespace UserIdentificationManager::Daemon
+{
+    namespace
+    {
+        IdSource::Group::Sources create_sources()
+        {
+            IdSource::Group::Sources sources;
+
+            // TODO: Create default sources.
+
+            return sources;
+        }
+    }
+
+    IdSource::IdSource(const std::string &name) : name_(name)
+    {
+    }
+
+    IdSource::~IdSource() = default;
+
+    void IdSource::user_identified(const IdentifiedUser &identified_user) const
+    {
+        if (listener_)
+            listener_->user_identified(identified_user);
+    }
+
+    IdSource::Group::Group() : Group(create_sources())
+    {
+    }
+
+    IdSource::Group::Group(Sources &&sources) : sources_(std::move(sources))
+    {
+        for (auto &source : sources_)
+            source->set_listener(this);
+    }
+
+    std::vector<std::string> IdSource::Group::enabled_names() const
+    {
+        std::vector<std::string> names;
+
+        for (auto &source : sources_)
+            if (source->enabled())
+                names.emplace_back(source->name());
+
+        return names;
+    }
+
+    std::vector<std::string> IdSource::Group::disabled_names() const
+    {
+        std::vector<std::string> names;
+
+        for (auto &source : sources_)
+            if (!source->enabled())
+                names.emplace_back(source->name());
+
+        return names;
+    }
+
+    void IdSource::Group::enable_all() const
+    {
+        for (auto &source : sources_)
+            source->enable();
+    }
+
+    void IdSource::Group::disable_all() const
+    {
+        for (auto &source : sources_)
+            source->disable();
+    }
+
+    void IdSource::Group::enable(const std::vector<std::string> &names) const
+    {
+        if (names.empty()) {
+            enable_all();
+            return;
+        }
+
+        disable_all();
+
+        for (const std::string &name : names) {
+            IdSource *source = find_source(name);
+
+            if (source)
+                source->enable();
+            else
+                g_warning("Can not enable \"%s\", unknown source", name.c_str());
+        }
+    }
+
+    std::vector<IdSource::IdentifiedUser> IdSource::Group::identified_users() const
+    {
+        return {identified_users_.cbegin(), identified_users_.cend()};
+    }
+
+    void IdSource::Group::user_identified(const IdentifiedUser &identified_user)
+    {
+        g_message("User identified, user identification id: %s, seat id: 0x%04x",
+                  identified_user.user_identification_id.c_str(),
+                  identified_user.seat_id);
+
+        if (identified_users_.size() >= MAX_SAVED_IDENTIFIED_USERS)
+            identified_users_.pop_front();
+
+        identified_users_.push_back(identified_user);
+
+        user_identified_signal_.emit(identified_user);
+    }
+
+    IdSource *IdSource::Group::find_source(const std::string &name) const
+    {
+        for (auto &source : sources_)
+            if (g_ascii_strcasecmp(source->name().c_str(), name.c_str()) == 0)
+                return source.get();
+
+        return nullptr;
+    }
+}
---
+++ src/daemon/unit_tests/id_source_test.cpp
@@ -0,0 +1,273 @@
+// Copyright (C) 2019 Luxoft Sweden AB
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#include "daemon/id_source.h"
+
+#include <gtest/gtest.h>
+
+#include <cstddef>
+#include <string>
+#include <vector>
+
+#include "common/scoped_silent_log_handler.h"
+
+namespace UserIdentificationManager::Daemon
+{
+    namespace
+    {
+        class TestSource : public IdSource
+        {
+        public:
+            explicit TestSource(unsigned int i) : IdSource("TEST" + std::to_string(i))
+            {
+            }
+
+            void enable() override
+            {
+                set_enabled(true);
+            }
+
+            void disable() override
+            {
+                set_enabled(false);
+            }
+
+            IdentifiedUser simulate_user_identified(const std::string &user_identification_id,
+                                                    IdSource::SeatId seat_id) const
+            {
+                IdentifiedUser user;
+
+                user.user_identification_id = name() + "-" + user_identification_id;
+                user.seat_id = seat_id;
+
+                user_identified(user);
+
+                return user;
+            }
+        };
+
+        class IdSourceGroupTest : public testing::Test
+        {
+        public:
+            using Names = std::vector<std::string>;
+
+            static constexpr unsigned int NUM_TEST_SOURCES = 3;
+
+            IdSourceGroupTest() : group_(create_test_sources())
+            {
+            }
+
+            TestSource &test_source(unsigned int num) const
+            {
+                return *sources_.at(num);
+            }
+
+            IdSource::Group &group()
+            {
+                return group_;
+            }
+
+        private:
+            IdSource::Group::Sources create_test_sources()
+            {
+                IdSource::Group::Sources sources;
+
+                for (unsigned int i = 0; i < NUM_TEST_SOURCES; i++) {
+                    auto source = std::make_unique<TestSource>(i + 1);
+                    sources_.emplace_back(source.get());
+                    sources.emplace_back(std::move(source));
+                }
+
+                return sources;
+            }
+
+            Common::ScopedSilentLogHandler log_handler_;
+            std::vector<TestSource *> sources_;
+            IdSource::Group group_;
+        };
+
+        testing::AssertionResult expect_user(const char *user1_expr,
+                                             const char *user2_expr,
+                                             const IdSource::IdentifiedUser &user1,
+                                             const IdSource::IdentifiedUser &user2)
+        {
+            if (user1.user_identification_id != user2.user_identification_id) {
+                return testing::AssertionFailure()
+                       << user1_expr << ".user_identification_id (\""
+                       << user1.user_identification_id << "\") != " << user2_expr
+                       << ".user_identification_id (\"" << user2.user_identification_id << "\")";
+            }
+
+            if (user1.seat_id != user2.seat_id) {
+                return testing::AssertionFailure()
+                       << user1_expr << ".seat_id (" << user1.seat_id << ") != " << user2_expr
+                       << ".seat_id (" << user2.seat_id << ")";
+            }
+
+            return testing::AssertionSuccess();
+        }
+
+        testing::AssertionResult expect_users(const char *users1_expr,
+                                              const char *users2_expr,
+                                              const std::vector<IdSource::IdentifiedUser> &users1,
+                                              const std::vector<IdSource::IdentifiedUser> &users2)
+        {
+            if (users1.size() != users2.size()) {
+                return testing::AssertionFailure()
+                       << users1_expr << ".size() (" << users1.size() << ") != " << users2_expr
+                       << ".size() (" << users2.size() << ")";
+            }
+
+            for (std::size_t i = 0; i < users1.size(); i++) {
+                testing::AssertionResult result =
+                    expect_user((std::string(users1_expr) + "[" + std::to_string(i) + "]").c_str(),
+                                (std::string(users2_expr) + "[" + std::to_string(i) + "]").c_str(),
+                                users1[i],
+                                users2[i]);
+
+                if (!result)
+                    return result;
+            }
+
+            return testing::AssertionSuccess();
+        }
+    }
+
+    TEST_F(IdSourceGroupTest, NoneEnabledByDefault)
+    {
+        EXPECT_TRUE(group().enabled_names().empty());
+        EXPECT_EQ(NUM_TEST_SOURCES, group().disabled_names().size());
+    }
+
+    TEST_F(IdSourceGroupTest, EnableAll)
+    {
+        group().enable_all();
+        EXPECT_EQ(NUM_TEST_SOURCES, group().enabled_names().size());
+        EXPECT_TRUE(group().disabled_names().empty());
+    }
+
+    TEST_F(IdSourceGroupTest, DisableAll)
+    {
+        group().enable_all();
+        EXPECT_EQ(NUM_TEST_SOURCES, group().enabled_names().size());
+
+        group().disable_all();
+        EXPECT_TRUE(group().enabled_names().empty());
+        EXPECT_EQ(NUM_TEST_SOURCES, group().disabled_names().size());
+    }
+
+    TEST_F(IdSourceGroupTest, EnableWithEmptyNamesArgumentEnablesAll)
+    {
+        group().enable({});
+        EXPECT_EQ(NUM_TEST_SOURCES, group().enabled_names().size());
+        EXPECT_TRUE(group().disabled_names().empty());
+    }
+
+    TEST_F(IdSourceGroupTest, EnableSpecificSources)
+    {
+        group().enable({"TEST1"});
+        EXPECT_EQ(Names({"TEST1"}), group().enabled_names());
+        EXPECT_EQ(Names({"TEST2", "TEST3"}), group().disabled_names());
+
+        group().enable({"TEST1", "TEST3"});
+        EXPECT_EQ(Names({"TEST1", "TEST3"}), group().enabled_names());
+        EXPECT_EQ(Names({"TEST2"}), group().disabled_names());
+
+        group().enable({"TEST2"});
+        EXPECT_EQ(Names({"TEST2"}), group().enabled_names());
+        EXPECT_EQ(Names({"TEST1", "TEST3"}), group().disabled_names());
+
+        group().enable({"TEST3"});
+        EXPECT_EQ(Names({"TEST3"}), group().enabled_names());
+        EXPECT_EQ(Names({"TEST1", "TEST2"}), group().disabled_names());
+
+        group().enable({"TEST1", "TEST2", "TEST3"});
+        EXPECT_EQ(Names({"TEST1", "TEST2", "TEST3"}), group().enabled_names());
+        EXPECT_TRUE(group().disabled_names().empty());
+    }
+
+    TEST_F(IdSourceGroupTest, EnableIsCaseInsensitive)
+    {
+        group().enable({"test1"});
+        EXPECT_EQ(Names({"TEST1"}), group().enabled_names());
+
+        group().enable({"TeSt2", "tEsT3"});
+        EXPECT_EQ(Names({"TEST2", "TEST3"}), group().enabled_names());
+        EXPECT_EQ(Names({"TEST1"}), group().disabled_names());
+    }
+
+    TEST_F(IdSourceGroupTest, EnableUnknownNamesAreIgnored)
+    {
+        group().enable({"UNKNOWN"});
+        EXPECT_TRUE(group().enabled_names().empty());
+
+        group().enable({"UNKNOWN1", "TEST1", "UNKNOWN2"});
+        EXPECT_EQ(Names({"TEST1"}), group().enabled_names());
+
+        group().enable({"UNKNOWN1", "TEST1", "UNKNOWN2", "TEST3"});
+        EXPECT_EQ(Names({"TEST1", "TEST3"}), group().enabled_names());
+    }
+
+    TEST_F(IdSourceGroupTest, UserIdentifiedSignal)
+    {
+        IdSource::IdentifiedUser received_user;
+        IdSource::IdentifiedUser expected_user;
+
+        group().enable_all();
+        group().user_identified_signal().connect([&](const auto &user) { received_user = user; });
+
+        expected_user = test_source(0).simulate_user_identified("123", 0x0123);
+        EXPECT_PRED_FORMAT2(expect_user, expected_user, received_user);
+
+        expected_user = test_source(1).simulate_user_identified("456", 0x4567);
+        EXPECT_PRED_FORMAT2(expect_user, expected_user, received_user);
+
+        expected_user = test_source(2).simulate_user_identified("789", 0x89ab);
+        EXPECT_PRED_FORMAT2(expect_user, expected_user, received_user);
+    }
+
+    TEST_F(IdSourceGroupTest, IdentifiedUsers)
+    {
+        std::vector<IdSource::IdentifiedUser> expected_users;
+
+        group().enable_all();
+
+        expected_users.emplace_back(test_source(0).simulate_user_identified("123", 0x0123));
+        EXPECT_PRED_FORMAT2(expect_users, expected_users, group().identified_users());
+
+        expected_users.emplace_back(test_source(1).simulate_user_identified("456", 0x4567));
+        EXPECT_PRED_FORMAT2(expect_users, expected_users, group().identified_users());
+    }
+
+    TEST_F(IdSourceGroupTest, IdentifiedUsersMoreThanMaxSavedDiscardsOld)
+    {
+        std::vector<IdSource::IdentifiedUser> expected_users;
+
+        group().enable_all();
+
+        for (unsigned int i = 0; i < IdSource::Group::MAX_SAVED_IDENTIFIED_USERS; i++) {
+            TestSource &source = test_source(i % NUM_TEST_SOURCES);
+
+            expected_users.emplace_back(
+                source.simulate_user_identified(std::to_string(i), IdSource::SeatId(i)));
+        }
+
+        EXPECT_EQ(IdSource::Group::MAX_SAVED_IDENTIFIED_USERS, group().identified_users().size());
+        EXPECT_PRED_FORMAT2(expect_users, expected_users, group().identified_users());
+
+        expected_users.emplace_back(test_source(0).simulate_user_identified("123", 0x0123));
+        expected_users.erase(expected_users.begin());
+        EXPECT_EQ(IdSource::Group::MAX_SAVED_IDENTIFIED_USERS, group().identified_users().size());
+        EXPECT_PRED_FORMAT2(expect_users, expected_users, group().identified_users());
+
+        expected_users.emplace_back(test_source(1).simulate_user_identified("456", 0x4567));
+        expected_users.erase(expected_users.begin());
+        EXPECT_EQ(IdSource::Group::MAX_SAVED_IDENTIFIED_USERS, group().identified_users().size());
+        EXPECT_PRED_FORMAT2(expect_users, expected_users, group().identified_users());
+    }
+}
